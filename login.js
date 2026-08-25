2ad98e6268d9c7e6cb21cbf6597a4f1615a08c2e
Moreen Ehab <148700336+MOREENehab@users.noreply.github.com>
2026-08-25 22:24:09 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,33 @@
+const signBtn = document.querySelector("#signBtn");
+
+const loginBox = document.querySelector("#loginBox");
+
+const successBox = document.querySelector("#successBox");
+
+const email = document.querySelector("#email");
+
+const password = document.querySelector("#password");
+
+
+signBtn.addEventListener("click", function () {
+
+    if (email.value == "") {
+
+        alert("Please enter your email");
+
+        return;
+    }
+
+
+    if (password.value == "") {
+
+        alert("Please enter your password");
+
+        return;
+    }
+
+
+    loginBox.style.display = "none"; 
+    successBox.style.display = "block";
+
+});
